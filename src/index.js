604480076739b4a405752c97dfb382b50123ad7a
Sub2n <95su1208@gmail.com>
2020-02-09 17:16:35 +0900

    feat: added Redux in project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,12 +1,36 @@
+// Action Types
+const SIGNIN = 'auth/SIGNIN';
+const SIGNOUT = 'auth/SIGNOUT';
+
+// Action Creators
+export const signin = () => ({ type: SIGNIN });
+export const signout = () => ({ type: SIGNOUT });
+
 const users = [
   { email: 'subin', password: '1234', name: '수빈' },
   { email: 'jisop', password: '1234', name: '지섭' },
 ];
 
-export function signIn({ email, password }) {
-  const user = users.find(
-    user => user.email === email && user.password === password,
-  );
-  if (user === undefined) throw new Error();
-  return user;
+const initialState = {
+  user: null,
+};
+
+function auth(state = initialState, action) {
+  switch (action.type) {
+    case SIGNIN:
+      return {
+        user: users.find(
+          user =>
+            user.email === action.email && user.password === action.password,
+        ),
+      };
+    case SIGNOUT:
+      return {
+        user: null,
+      };
+    default:
+      return state;
+  }
 }
+
+export default auth;
