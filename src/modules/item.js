43bbfcdf555cf54f41ff855226168191730ccc19
Sub2n <95su1208@gmail.com>
2020-02-17 20:08:53 +0900

    chore: Add dotenv for apiUrl ENV

---
+++ src/modules/item.js
@@ -0,0 +1,12 @@
+import { createAction } from 'redux-actions';
+
+// Action Types
+const SEARCH = 'item/SEARCH';
+const SEARCH_SUCCESS = 'item/SEARCH_SUCCESS';
+
+const GET_ITEM = 'item/GET_ITEM';
+const GET_ITEM_SUCCESS = 'item/GET_ITEM_SUCCESS';
+
+// Action Creators
+export const search = createAction(SEARCH, query => query);
+export const getItem = createAction(GET_ITEM, id => id);
