fb33d8d5cfc271807d4e7ab72d947dbb7ca19a4b
JiSop <ddie0821@gmail.com>
2020-03-01 21:45:09 +0900

    feat: Add ContentSection component title
    ContentSection component에 type 어트리뷰트를 사용하여
    카테고리별 상품 조회 페이지와 검색결과 페이지에 맞게 렌더링 할수 있도록 만들었습니다

--- src/components/common/ContentSection.js
+++ src/components/common/ContentSection.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import Elevation from '../../styles/Elevation';
+import Colors from '../../styles/Colors';
 
 const ContentSectionBlock = styled.section`
   margin: 1rem auto 0;
-  padding: 24px;
+  padding-top: 1.5rem;
   max-width: 1024px;
   border-top-left-radius: 15px;
   border-top-right-radius: 15px;
@@ -20,17 +21,36 @@ const Title = styled.div`
   justify-content: center;
   flex-direction: column;
   padding-left: 16px;
+
+  & h1 {
+    width: 375px;
+    font-size: 48px;
+    line-height: 48px;
+    color: ${Colors.onSurfaceHigh};
+  }
+
+  & h2 {
+    color: ${Colors.primary};
+  }
 `;
 
-const ContentSection = ({ children, title }) => {
+const textMap = {
+  category: '의 추천 상품',
+  search: '의 검색 결과입니다.',
+};
+
+const ContentSection = ({ type, title, children }) => {
+  const text = textMap[type];
   return (
     <>
-      {title ? (
+      {title && (
         <Title>
-          <h1>{title}</h1>
+          {type === 'category' && <h1>{title}</h1>}
+          <h2>
+            {title}
+            {text}
+          </h2>
         </Title>
-      ) : (
-        ''
       )}
       <ContentSectionBlock>{children}</ContentSectionBlock>
     </>
