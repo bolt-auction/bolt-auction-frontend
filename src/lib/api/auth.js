e4c774c1a1b5e6baa99657e39ec03a86bc96f705
JiSop <ddie0821@gmail.com>
2020-03-16 22:10:50 +0900

    fix: signin
    처음 로그인할때 요청이 제대로 이루어지지 않는 문제를 해결하였습니다.

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -20,4 +20,17 @@ export const signup = ({ uid, passwd, name }) =>
 /**
  * 회원정보 조회 (authorization)
  */
-export const check = () => client.get('/api/member');
+export const check = () =>
+  client.get('/api/member', {
+    headers: {
+      Authorization: `Bearer ${localStorage.token}`,
+    },
+  });
+/**
+ * NOTE:
+ * 처음 로그인할때 디폴트 헤더에 토큰이 없기 때문에 (localStorage.token === undefined)
+ * 요청이 제대로 이루어지지 않는 문제를 해결하기 위해 check에서만 헤더를 삽입
+ * FIXME:
+ * check에 리덕스 스토어에 있는 accessToken을 전달해서 사용하도록 변경하거나
+ * axios.defaults.headers.common['Authorization']을 여기서 선언해 전달 하도록 변경
+ */
