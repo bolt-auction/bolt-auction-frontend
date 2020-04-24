933ea842edf954a43de154f476814825a5f2e318
JiSop <ddie0821@gmail.com>
2020-04-25 01:31:41 +0900

    refactor: Convert connect to useSelector
    SigninContainer, SignupContainer, SellContainer의 connect를 useSelector로 변경

--- src/containers/SellContainer.js
+++ src/containers/SellContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import Sell from '../components/sell/Sell';
@@ -19,96 +19,114 @@ import { calEndTime } from '../lib/util';
  *  [x]업로드할 이미지 미리보기
  *  [x]상품이미지 업로드 4개로 제한
  *  [x]원하는 이미지 삭제 하기
- *  []밸리데이션 추가
- *    []숫자만 존재하는지
+ *  [x]밸리데이션 추가
+ *    [x]숫자
  *    [x]모든 양식이 채워졌는지
  *  []에러 메시지 랜더링
  */
 
-const SellContainer = withRouter(
-  ({
-    history,
-    categoryList,
-    changeField,
+const SellContainer = ({ history }) => {
+  const [previewImages, setPreviewImages] = useState([]);
+  const dispatch = useDispatch();
+  const {
     sellForm,
     images,
-    initializeForm,
-    sellProduct,
-    changeFile,
-    loading,
     error,
     itemId,
-  }) => {
-    const [previewImages, setPreviewImages] = useState([]);
-    /**
-     * 이미지 파일 핸들러
-     * @param {array} imageFiles
-     */
-    const imageFilesHandler = imageFiles => {
-      // 이미지를 선택 안하고 취소했을시 초기화, 마지막 파일을 제거했을때 imgBase64 초기화
-      if (imageFiles.length === 0) {
-        setPreviewImages([]);
-        changeFile([]);
-        return;
-      }
-      // 프리뷰 이미지로 사용하기 위한 인코딩 객체 생성
-      (function() {
-        let images = [];
-        imageFiles.forEach((file, i) => {
-          let reader = new FileReader();
-          reader.onloadend = () => {
-            images = [
-              ...images,
-              {
-                name: `${i}${file.name}`,
-                base64: reader.result,
-              },
-            ];
-            setPreviewImages(images);
-          };
-          reader.readAsDataURL(file);
-        });
-      })();
-      // 리덕스의 이미지 데이터 변경
-      changeFile(imageFiles);
-    };
+    categoryList,
+    loading,
+  } = useSelector(({ sell, category, loading }) => ({
+    sellForm: sell.sellForm,
+    images: sell.sellForm.images,
+    error: sell.error,
+    itemId: sell.itemId,
+    categoryList: category.categories.supCategoryList,
+    loading: loading['sell/SELL_PRODUCT'],
+  }));
+  /**
+   * 이미지 파일 핸들러
+   * @param {array} imageFiles
+   */
+  const imageFilesHandler = imageFiles => {
+    // 이미지를 선택 안하고 취소했을시 초기화, 마지막 파일을 제거했을때 imgBase64 초기화
+    if (imageFiles.length === 0) {
+      setPreviewImages([]);
+      dispatch(changeFile([]));
+      return;
+    }
+    // 프리뷰 이미지로 사용하기 위한 인코딩 객체 생성
+    (function() {
+      let images = [];
+      imageFiles.forEach((file, i) => {
+        let reader = new FileReader();
+        reader.onloadend = () => {
+          images = [
+            ...images,
+            {
+              name: `${i}${file.name}`,
+              base64: reader.result,
+            },
+          ];
+          setPreviewImages(images);
+        };
+        reader.readAsDataURL(file);
+      });
+    })();
+    // 리덕스의 이미지 데이터 변경
+    dispatch(changeFile(imageFiles));
+  };
 
-    const onChangeFile = e => {
-      const { files } = e.target;
-      const imageFiles = [...files, ...images];
-      // 선택한 이미지의 개수가 4개 이상일 경우 경고 메시지
-      if (imageFiles.length > 4) {
-        e.target.value = '';
-        return alert('최대 4개의 이미지만 업로드 가능합니다.');
-      }
-      imageFilesHandler(imageFiles);
-    };
+  const onChangeFile = e => {
+    const { files } = e.target;
+    const imageFiles = [...files, ...images];
+    // 선택한 이미지의 개수가 4개 이상일 경우 경고 메시지
+    if (imageFiles.length > 4) {
+      e.target.value = '';
+      return alert('최대 4개의 이미지만 업로드 가능합니다.');
+    }
+    imageFilesHandler(imageFiles);
+  };
 
-    const onRemoveImage = name => {
-      const imageFiles = images.filter(
-        (image, i) => `${i}${image.name}` !== name,
-      );
-      imageFilesHandler(imageFiles);
-    };
+  const onRemoveImage = name => {
+    const imageFiles = images.filter(
+      (image, i) => `${i}${image.name}` !== name,
+    );
+    imageFilesHandler(imageFiles);
+  };
 
-    const onChange = e => {
-      const { value, name } = e.target;
-      // 상위 카테고리가 변경될 경우 세부 카테고리를 초기화
-      if (name === 'supCategoryId') {
+  const onChange = e => {
+    const { value, name } = e.target;
+    // 상위 카테고리가 변경될 경우 세부 카테고리를 초기화
+    if (name === 'supCategoryId') {
+      dispatch(
         changeField({
           key: 'categoryId',
           value: '',
-        });
-      }
+        }),
+      );
+    }
+    dispatch(
       changeField({
         key: name,
         value,
-      });
-    };
+      }),
+    );
+  };
 
-    const onSubmit = e => {
-      e.preventDefault();
-      const {
+  const onSubmit = e => {
+    e.preventDefault();
+    const {
+      categoryId,
+      name,
+      startPrice,
+      quickPrice,
+      minBidPrice,
+      description,
+      endDt,
+      images,
+    } = sellForm;
+    if (
+      [
         categoryId,
         name,
         startPrice,
@@ -117,22 +135,12 @@ const SellContainer = withRouter(
         description,
         endDt,
         images,
-      } = sellForm;
-      if (
-        [
-          categoryId,
-          name,
-          startPrice,
-          quickPrice,
-          minBidPrice,
-          description,
-          endDt,
-          images,
-        ].includes('')
-      ) {
-        alert('모든 양식을 입력해주세요.');
-        return;
-      }
+      ].includes('')
+    ) {
+      alert('모든 양식을 입력해주세요.');
+      return;
+    }
+    dispatch(
       sellProduct({
         categoryId,
         name,
@@ -142,54 +150,39 @@ const SellContainer = withRouter(
         description,
         endDt: calEndTime(endDt),
         images,
-      });
-    };
-
-    useEffect(() => {
-      return () => {
-        initializeForm();
-      };
-    }, [initializeForm]);
-
-    useEffect(() => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      if (itemId) {
-        history.push(`/products/${itemId}`);
-      }
-    }, [error, itemId, history]);
-
-    return (
-      <Sell
-        sellForm={sellForm}
-        images={images}
-        onChange={onChange}
-        onChangeFile={onChangeFile}
-        onSubmit={onSubmit}
-        loading={loading}
-        categoryList={categoryList}
-        previewImages={previewImages}
-        onRemoveImage={onRemoveImage}
-      />
+      }),
     );
-  },
-);
+  };
 
-export default connect(
-  ({ sell, category, loading }) => ({
-    sellForm: sell.sellForm,
-    images: sell.sellForm.images,
-    error: sell.error,
-    itemId: sell.itemId,
-    categoryList: category.categories.supCategoryList,
-    loading: loading['sell/SELL_PRODUCT'],
-  }),
-  {
-    changeField,
-    initializeForm,
-    sellProduct,
-    changeFile,
-  },
-)(SellContainer);
+  useEffect(() => {
+    return () => {
+      dispatch(initializeForm());
+    };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    if (itemId) {
+      history.push(`/products/${itemId}`);
+    }
+  }, [error, itemId, history]);
+
+  return (
+    <Sell
+      sellForm={sellForm}
+      images={images}
+      onChange={onChange}
+      onChangeFile={onChangeFile}
+      onSubmit={onSubmit}
+      loading={loading}
+      categoryList={categoryList}
+      previewImages={previewImages}
+      onRemoveImage={onRemoveImage}
+    />
+  );
+};
+
+export default withRouter(SellContainer);
--- src/containers/auth/SignupContainer.js
+++ src/containers/auth/SignupContainer.js
@@ -1,141 +1,141 @@
 import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { changeField, initializeForm, signup } from '../../modules/auth';
-import AuthForm from '../../components/auth/AuthForm';
 import { withRouter } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeField, initializeForm, signup } from '../../modules/auth';
 import { validation } from '../../lib/util';
+import AuthForm from '../../components/auth/AuthForm';
 
-const SignupForm = withRouter(
-  ({
-    history,
-    form,
-    auth,
-    authError,
-    changeField,
-    initializeForm,
-    signup,
-    loading,
-  }) => {
-    const [error, setError] = useState(null);
+const SignupForm = ({ history }) => {
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  const { form, auth, authError, loading } = useSelector(
+    ({ auth, loading }) => ({
+      form: auth.signup,
+      auth: auth.auth,
+      authError: auth.authError,
+      loading: loading['auth/SIGNUP'],
+    }),
+  );
 
-    // 인풋 변경 이벤트 핸들러
-    const onChange = e => {
-      const { value, name } = e.target;
-      if (error) {
-        setError(null);
-      }
+  // 인풋 변경 이벤트 핸들러
+  const onChange = e => {
+    const { value, name } = e.target;
+    if (error) {
+      setError(null);
+    }
+    dispatch(
       changeField({
         form: 'signup',
         key: name,
         value,
-      });
-    };
+      }),
+    );
+  };
 
-    // NOTE: 에러 핸들링 hook 알아보기
-    // 폼 등록 이벤트 핸들러
-    const onSubmit = e => {
-      e.preventDefault();
-      const { uid, passwd, passwdConfirm, name } = form;
-      if ([uid, passwd, passwdConfirm, name].includes('')) {
-        setError('빈 칸을 모두 입력하세요.');
-        return;
-      }
-      if (validation('email', uid)) {
-        setError('올바른 이메일 형식이 아닙니다.');
-        return;
-      }
-      if (validation('password', passwd)) {
-        setError('비밀번호는 문자 또는 숫자 6~12자리 이어야합니다.');
+  // NOTE: 에러 핸들링 hook 알아보기
+  // 폼 등록 이벤트 핸들러
+  const onSubmit = e => {
+    e.preventDefault();
+    const { uid, passwd, passwdConfirm, name } = form;
+    if ([uid, passwd, passwdConfirm, name].includes('')) {
+      setError('빈 칸을 모두 입력하세요.');
+      return;
+    }
+    if (validation('email', uid)) {
+      setError('올바른 이메일 형식이 아닙니다.');
+      return;
+    }
+    if (validation('password', passwd)) {
+      setError('비밀번호는 문자 또는 숫자 6~12자리 이어야합니다.');
+      dispatch(
         changeField({
           form: 'signup',
           key: 'passwd',
           value: '',
-        });
+        }),
+      );
+      dispatch(
         changeField({
           form: 'signup',
           key: 'passwdConfirm',
           value: '',
-        });
-        return;
-      }
-      if (passwd !== passwdConfirm) {
-        setError('비밀번호가 일치하지 않습니다.');
+        }),
+      );
+      return;
+    }
+    if (passwd !== passwdConfirm) {
+      setError('비밀번호가 일치하지 않습니다.');
+      dispatch(
         changeField({
           form: 'signup',
           key: 'passwd',
           value: '',
-        });
+        }),
+      );
+      dispatch(
         changeField({
           form: 'signup',
           key: 'passwdConfirm',
           value: '',
-        });
-        return;
-      }
-      if (name.length < 3) {
-        setError('닉네임은 세 자리 이상이어야 합니다.');
+        }),
+      );
+      return;
+    }
+    if (name.length < 3) {
+      setError('닉네임은 세 자리 이상이어야 합니다.');
+      dispatch(
         changeField({
           form: 'signup',
           key: 'name',
           value: '',
-        });
-        return;
-      }
-      setError(null);
+        }),
+      );
+      return;
+    }
+    setError(null);
+    dispatch(
       signup({
         uid,
         passwd,
         name,
-      });
-    };
+      }),
+    );
+  };
 
-    // 컴포넌트가 처음 렌더링 될 때 form을 초기화함
-    useEffect(() => {
-      initializeForm('signup');
-    }, [initializeForm]);
+  // 컴포넌트가 처음 렌더링 될 때 form을 초기화함
+  useEffect(() => {
+    dispatch(initializeForm('signup'));
+  }, [dispatch]);
 
-    // TODO: []개인정보 이용 동의, 약관 동의 체크 버튼 추가 후 동의 여부 밸리데이션
-    // TODO: []회원가입 성공 후 로그인 페이지로 이동하기 전 성공했다는 메시지 보여주기
-    // NOTE: 회원가입 API response에 토큰이 없어 성공시에 로그인 페이지로 이동 시킴
-    useEffect(() => {
-      setError(null);
-      if (authError) {
-        if (authError.response.status === 403) {
-          setError('이미 사용중인 이메일입니다.');
-          return;
-        }
-        console.log(authError);
-        setError('회원가입에 실패하였습니다.');
+  // TODO: []개인정보 이용 동의, 약관 동의 체크 버튼 추가 후 동의 여부 밸리데이션
+  // TODO: []회원가입 성공 후 로그인 페이지로 이동하기 전 성공했다는 메시지 보여주기
+  // NOTE: 회원가입 API response에 토큰이 없어 성공시에 로그인 페이지로 이동 시킴
+  useEffect(() => {
+    setError(null);
+    if (authError) {
+      if (authError.response.status === 403) {
+        setError('이미 사용중인 이메일입니다.');
         return;
       }
-      if (auth) {
-        history.push('/signin');
-      }
-    }, [auth, authError, history]);
+      console.log(authError);
+      setError('회원가입에 실패하였습니다.');
+      return;
+    }
+    if (auth) {
+      history.push('/signin');
+    }
+  }, [auth, authError, history]);
 
-    return (
-      <AuthForm
-        type="signup"
-        form={form}
-        onChange={onChange}
-        onSubmit={onSubmit}
-        error={error}
-        loading={loading}
-      />
-    );
-  },
-);
+  return (
+    <AuthForm
+      type="signup"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      error={error}
+      loading={loading}
+    />
+  );
+};
 
-export default connect(
-  ({ auth, loading }) => ({
-    form: auth.signup,
-    auth: auth.auth,
-    authError: auth.authError,
-    loading: loading['auth/SIGNUP'],
-  }),
-  {
-    changeField,
-    initializeForm,
-    signup,
-  },
-)(SignupForm);
+export default withRouter(SignupForm);
